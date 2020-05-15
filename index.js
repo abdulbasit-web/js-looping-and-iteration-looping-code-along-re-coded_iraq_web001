f51349f662e0a20a375212811dff38590cc7e1a1
Abdulbasit Salah <abdullbasit99salah@gmail.com>
2020-05-15 21:43:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // // Code your solutions in this file
 function writeCards(names , eventName){
   for(let i=0; i<names.length;i++){
-    console.log(``);
+    console.log(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
   }
 }
+
+
